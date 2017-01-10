70f3e9e183c092f26a6b579693f8b0cbab83171b
Derek Villars and Tammy Dang <derek@>
2017-01-10 14:03:23 -0800

    add vocabulary bar and fix bugs to show all vocabulary and definitions properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
+
   $(".init-show").click(function() {
     $(this).slideToggle();
     $(this).next().slideToggle();
   });
+
   $('.init-hidden').click(function() {
     $(this).slideToggle();
     $(this).prev().slideToggle();
   });
-})
+
+  $("#showall").click(function() {
+    $(".init-hidden").slideDown();
+    $(".init-show").slideDown();
+  });
+
+  $("#def").click(function() {
+    $(".init-hidden").slideDown();
+    $(".init-show").slideUp();
+
+  });
+
+  $("#vocab").click(function() {
+    $(".init-hidden").slideUp();
+    $(".init-show").slideDown();
+
+  });
+});
